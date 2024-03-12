4b8b2c2f4e547ef470583b65e8e532405193d097
SeorinLee <yms9074@gmail.com>
2024-03-12 12:24:20 +1000

    Milestone 1: contribute Javascript

--- milestone1/App.js
+++ milestone1/App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.innercontainer}>
+       <Text style={styles.title}>My Todo List</Text>
+       <View style={styles.titleseparator}></View>
+       </View>
+       <View style={styles.todoList}>
+        <Text style={styles.todoItem}>Buy milk</Text>
+        <Text style={styles.todoItem}>Buy bread</Text>
+        <Text style={styles.todoItem}>Buy eggs</Text>
+        <View style={styles.listseparator}></View>
+        </View>
+        <TouchableOpacity style={styles.addButton}>
+        <Text style={styles.addButtonText}>Add New Todo</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  innercontainer: {
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'top',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 50, 
+  },
+  titleseparator: {
+    width: '90%',
+    height: 4, 
+    backgroundColor: 'black', 
+    marginTop: 15,
+  },
+  todoList: {
+    alignItems: 'flex-start', // 리스트를 왼쪽으로 정렬
+    marginTop: 10,
+    marginLeft: 20,
+  },
+  todoItem: {
+    fontSize: 18,
+    marginBottom: 10,
+    backgroundColor: 'rgb(176, 227, 255)',
+    fontSize: 15,
+    width: 350,
+    height: 35,
+    lineHeight: 35,
+    paddingLeft: 10,
+    borderRadius: 10,
+  },
+  listseparator:{
+    width: '94%',
+    height: 2, 
+    backgroundColor: 'black', 
+    marginTop: 450,
+  },
+  addButton: {
+    backgroundColor: 'rgb(2, 151, 250)',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 2,
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    alignItems: 'center',
+    elevation: 3, 
+  },
+  addButtonText:{
+  color: 'white', // 텍스트 색상
+  fontSize: 16, // 텍스트 크기
+  fontWeight: 'bold', // 텍스트 굵기
+  },
+
 });
